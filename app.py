1fb330b84dc56d101b756f8bfd325be7c6cf8daf
ARNAS STEPONAVICIUS <G00361891@gmit.ie>
2021-01-06 17:01:40 +0000

    add model to flask, send predicted values to frontend

--- app.py
+++ app.py
@@ -1,26 +1,41 @@
-from collections import defaultdict
-
 # flask for the web application
+from sys import modules
 import flask as fl
 # numpy for the numerical work
 import numpy as np
+# used to load keras model
+from tensorflow.keras.models import Sequential, save_model, load_model
 
-import csv
-
+# initialize the Flask app
 app = fl.Flask(__name__)
+
+def get_model():
+    global model
+    model = load_model('turbine_model.h5')
+    print('model loaded')
 
 # landing page
 @app.route('/')
 def index():
-    return fl.current_app.send_static_file('index.html')
+    return app.send_static_file('index.html')
 
 # api route that returns the csv dataset using numpy
 # adapted from https://pythonhealthcare.org/2018/04/04/25-reading-and-writing-csv-files-using-numpy-and-pandas/
-@app.route('/api/dataset')
-def show_dataset():
-    # load in the csv file
-    data = np.loadtxt('datasets/powerproduction.csv', delimiter=',', skiprows=1)
-    # add the data of the csv to a dictionary
-    dictionary = dict(data)
-    # return the dictionary
-    return dictionary
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    print('loading keras model')
+    get_model()
+
+    message = fl.request.get_json(force=True)
+    speed = message['speed']
+
+    prediction = model.predict([float(speed)]).tolist()
+
+    response = {
+        'prediction': { 
+            'power': prediction[0]
+        }
+    }
+
+    print(response)
+    return fl.jsonify(response)
