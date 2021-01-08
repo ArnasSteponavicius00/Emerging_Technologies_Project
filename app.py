f98c2d9fb3ad4e5aa3bdc9cf72ff31b8880916ad
ARNAS STEPONAVICIUS <G00361891@gmit.ie>
2021-01-08 15:07:35 +0000

    redo frontend for displaying the power prediction

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 # flask for the web application
 from sys import modules
-import flask as fl
-# numpy for the numerical work
-import numpy as np
+from flask import Flask, request, render_template
+
 # used to load keras model
-from tensorflow.keras.models import Sequential, save_model, load_model
+from tensorflow.keras.models import load_model
 
 # initialize the Flask app
-app = fl.Flask(__name__)
+app = Flask(__name__)
 
 def get_model():
     global model
@@ -15,27 +14,15 @@ def get_model():
     print('model loaded')
 
 # landing page
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
-
-# api route that returns the csv dataset using numpy
-# adapted from https://pythonhealthcare.org/2018/04/04/25-reading-and-writing-csv-files-using-numpy-and-pandas/
-@app.route('/predict', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def predict():
-    print('loading keras model')
-    get_model()
+    if request.method == 'POST':
+        print('loading keras model')
+        get_model()
 
-    message = fl.request.get_json(force=True)
-    speed = message['speed']
+        speed = request.form['speed']
 
-    prediction = model.predict([float(speed)]).tolist()
+        prediction = model.predict([float(speed)])
 
-    response = {
-        'prediction': { 
-            'power': prediction[0]
-        }
-    }
-
-    print(response)
-    return fl.jsonify(response)
+        return render_template('index.html', prediction=prediction[0])
+    return render_template('index.html')
